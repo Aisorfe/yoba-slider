eea13b14b33257bbd11b982c03da18ce48c9023a
Nikita Belousov <seriouscat1001@gmail.com>
2016-09-21 01:35:50 +0300

    most of animation now via css, sidebars added

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,172 +2,134 @@ var slider = (function($) {
 
     var _ = {
 
-        cls: {
-
-            viewer: ['viewer', '.viewer'],
-
-            slides_wrapper: ['viewer__slides-wrapper', '.viewer__slides-wrapper'],
-
-            slide: ['viewer__slide', '.viewer__slide'],
-
-            slide_content: ['viewer__slide-content', '.viewer__slide-content'],
-
-            slide_caption: ['viewer__slide-caption', '.viewer__slide-caption'],
-
-            slide_text: ['viewer__slide-text', '.viewer__slide-text'],
-
-            toggle: ['nav__toggle', '.nav__toggle'],
-
-            nav_next: ['nav__next', '.nav__next'],
-
-            nav_previous: ['nav__previous', '.nav__previous']
-
-        },
-
-        settings: {
-
-            // Должно совпадать с аналогичной настройкой в _vars.scss
-            preview_slide_size: [320, 260],
-
-            zoom_time: 600,
-
-            zoom_easing: 'easeOutCubic' 
-
-        },
-
-        $window: null,
-        $body: null,
-        $viewer: null,
-        $slides_wrapper: null,
-        $slide: null,
-        $num_of_slides: null,
-        $toggle: null,
-        $nav_next: null,
-        $nav_previous: null,
-        $current_slide: null,
+        $window         : null,
+        $body           : null,
+        $slider         : null,
+        $viewer         : null,
+        $slides_wrapper : null,
+        $slide          : null,
+        $navPrevious    : null,
+        $navNext        : null,
+        $sidebarL       : null,
+        $sidebarR       : null,
+        scroll_enabled  : null,
+        $num_of_slides  : null,
 
         init_properties: function() {
 
-            console.log('...init properties');
-
-            // elements
-            _.$window = $(window);
-            _.$body = $('body');
-            _.$viewer = $(_.cls.viewer[1]);
-            _.$slides_wrapper = $(_.cls.slides_wrapper[1]);
-            _.$slide = $(_.cls.slide[1]);
-            _.$num_of_slides = _.$slide.length;
-            _.$toggle = _.$viewer.find(_.cls.toggle[1]);
-            _.$navNext = _.$viewer.find(_.cls.nav_next[1]);
-            _.$navPrevious = _.$viewer.find(_.cls.nav_previous[1]);
+            // dom elements
+            _.$window         = $(window);
+            _.$body           = $('body');
+            _.$slider         = $('.slider');
+            _.$viewer         = $('.viewer');
+            _.$slides_wrapper = $('.slides-wrapper');
+            _.$slide          = $('.slide');
+            _.$navPrevious    = $('.previous');
+            _.$navNext        = $('.next');
+            _.$sidebarL       = $('.sidebar.left');
+            _.$sidebarR       = $('.sidebar.right');
 
             // other
             _.scroll_enabled = true;
+            _.$num_of_slides = _.$slide.length;
 
         },
 
         init_viewer: function() {
 
-            console.log('...init viewer');
+            // making some slides
+            _.$slides_wrapper.children().each(function() {
 
-            _.$slides_wrapper.children()
-                .each(function() {
+                var s = $(this);
 
-                    var $this = $(this),
-                        slide;
+                $('<div class="content"></div>').appendTo(s);
 
-                    slide = {
-                        $image_data: $this.attr('data-image'),
-                        $caption_data: $this.attr('data-caption'),
-                        $text_data: $this.attr('data-text'),
-                        loaded: false
-                    };
+                $('<div class="caption"></div>').append(s.attr('data-caption')).
+                                                 appendTo(s.children('.content'));
+                $('<div class="text"></div>').append(s.attr('data-text')).
+                                                 appendTo(s.children('.content'));
 
-                    slide.$content = $('<div class="' +  _.cls.slide_content[0] + '"></div>')
-                        .appendTo($this);
+                s.css('background-image', 'url(' +  s.attr('data-image') +')');
 
-                    slide.$caption = $('<div class="' +  _.cls.slide_caption[0] + '"></div>')
-                        .append(slide.$caption_data)
-                        .appendTo($this.children(_.cls.slide_content[1]));
-
-                    slide.$text = $('<div class="' +  _.cls.slide_text[0] + '"></div>')
-                        .append(slide.$text_data)
-                        .appendTo($this.children(_.cls.slide_content[1]));
-
-                    $this.css('background-image', 'url(' + slide.$image_data + ')');
-
-                }) 
+            }) 
 
         },
 
         init_events: function() {
 
-            console.log('...init events');
-
             _.$window.on('load', function() {
                 _.$body.removeClass('is-loading');
             });
 
+            // scrolling via mouse hovering
+            _.init_scrolling();
+
+            // zoom-in
             _.$slide.click(function() {
-
-                if (_.$viewer.hasClass('viewer--zoom-out')) {
-
+                if (_.$slider.hasClass('zoomed-out')) {
                     _.scroll_enabled = false;
                     _.$body.css({ overflow: 'hidden' });
                     _.$current_slide = $(this);
                     $(this).addClass('current');
                     _.zoom('in');
-
                 }
-
             });
 
-            $(_.cls.nav_next[1]).click(function() {
-                if (_.$viewer.hasClass('is-loading') || _.$current_slide.index() == _.$num_of_slides - 1) {
-                    return;
-                }
-                _.switch('next');
+            // zoom-out
+            $('.all-slides').click(function() {
+                $('.sidebar').each(function() {
+                    $(this).removeClass('shown');
+                    _.$slider.removeClass('zoomed-in');
+                    setTimeout(function() {
+                        _.zoom('out');
+                    }, 600);
+                })
             });
 
-            $(_.cls.nav_previous[1]).click(function() {
-                if (_.$viewer.hasClass('is-loading') || _.$current_slide.index() == 0) {
-                    return;
-                }
+            // sidebar appearance
+            var f = function() {
+                var self = $(this);
+                setTimeout(function() {
+                    var sbar = self.attr('class') == 'previous' ? _.$sidebarL : _.$sidebarR;
+                    sbar.addClass('shown');
+                }, 300)
+            }
+
+            _.$navNext.hover(f);
+            _.$navPrevious.hover(f);
+
+            // switch slide
+            _.$sidebarL.children('.preview').click(function() {
+                if (_.$current_slide.index() == 0) return;
                 _.switch('previous');
             });
 
-            $(_.cls.toggle[1]).click(function() {
-                _.zoom('out');
+            _.$sidebarR.children('.preview').click(function() {
+                if (_.$current_slide.index() == _.$num_of_slides - 1) return;
+                _.switch('next');
             });
-
-            _.set_mouse_scrolling();
 
         },
 
-        set_mouse_scrolling: function() {
+
+        init_scrolling: function() {
 
             var delta = 0;
 
             _.$slide.on('mousemove', function(e) {
                 delta = (e.clientX - window.innerWidth / 2) * 0.05;
             });
-
             _.$slide.on('mouseleave blur', function() {
                 delta = 0;
             });
 
             (function f() {
-
                 if (delta && _.scroll_enabled) {
-
                     _.$window.scrollLeft(function(i, v) {
                         return v + delta;
                     });
-
                 }
-
                 webkitRequestAnimationFrame(f);
-
             })();
 
         },
@@ -187,13 +149,11 @@ var slider = (function($) {
             var offset = (_.$current_slide.index() + i) * -100 + (_.$window.scrollLeft() * 100 / document.documentElement.clientWidth) + 'vw';
 
             _.animate_offset(offset, function() {
-
                 var new_current = _.$slides_wrapper.children().eq(_.$current_slide.index() + i);
-                _.animate_slide_content(new_current, 'appear');
-                _.animate_slide_content(_.$current_slide, 'disappear');
+                // _.animate_slide_content(new_current, 'appear');
+                // _.animate_slide_content(_.$current_slide, 'disappear');
                 _.$current_slide = new_current;
                 _.$viewer.removeClass('is-loading');
-
             });
 
         },
@@ -207,7 +167,20 @@ var slider = (function($) {
 
                 _.animate_offset(offset);
                 _.animate_slides_size('100vw', '100vh');
-                _.$viewer.removeClass('viewer--zoom-out');
+                _.$slider.removeClass('zoomed-out');
+
+                setTimeout(function() {
+                    _.$slider.addClass('zoomed-in');
+                }, 600);
+
+            } else if (mode == 'out') {
+
+                _.$slider.addClass('zoomed-out');
+                _.animate_slides_size(320, 260);
+                _.animate_offset(0, function() {
+                    _.scroll_enabled = true;
+                });
+                _.$body.css('overflow', 'auto');
 
             }
 
@@ -218,8 +191,8 @@ var slider = (function($) {
             _.$viewer.animate({ 
                 marginLeft: offset
             }, {
-                duration: _.settings.zoom_time,
-                easing: _.settings.zoom_easing,
+                duration: 600,
+                easing: 'easeOutCubic',
                 complete: callback
             });
 
@@ -231,69 +204,13 @@ var slider = (function($) {
                 width: width,
                 height: height
             }, {
-                duration: _.settings.zoom_time,
-                easing: _.settings.zoom_easing,
-            });
-
-            setTimeout(function() {
-
-                _.animate_slide_content(_.$current_slide, 'appear');
-                _.animate_navigation();
-
-            }, _.settings.zoom_time);
-
-        },
-
-        animate_slide_content: function(slide, mode) {
-
-            var o, t;
-
-            if (mode == 'appear') {
-
-                o = 1;
-                t = '0';
-
-            } else if (mode == 'disappear') {
-
-                o = 0;
-                t = '40px';
-
-            }
-
-            slide.children(_.cls.slide_content[1]).animate({
-                opacity: o,
-                top: t
-            }, {
-                duration: 400,
-                easing: 'easeOutCubic'
-            });
-
-        },
-
-        animate_navigation: function() {
-
-            var options = {
-                duration: 800,
+                duration: 600,
                 easing: 'easeOutCubic',
-            }
-
-            $('.nav').animate({
-                opacity: 1
-            }, options);
-
-            $('.nav__previous').animate({
-                marginLeft: '.8em'
-            }, options);
-
-            $('.nav__next').animate({
-                marginRight: '.8em'
-            }, options);
+            });
 
         },
 
         init: function() {
-
-            console.log('init started');
 
             skel.breakpoints({
                 xlarge: '(max-width: 1680px)',
